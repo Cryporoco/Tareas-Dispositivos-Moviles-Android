1a4e7386579d31af6ba7c027315620df336840c8
Sebastian David <sebdavidpiza@gmail.com>
2024-04-13 23:55:32 -0500

    Reproductor

--- Ejercicios Semana 3/Reproductor/app/src/main/java/com/example/reproductor/MainActivity.kt
+++ Ejercicios Semana 3/Reproductor/app/src/main/java/com/example/reproductor/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.reproductor
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,25 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var  cancion = MediaPlayer.create(this,R.raw.ratriver)
+        var play = findViewById<Button>(R.id.Play)
+        var pause = findViewById<Button>(R.id.Pausar)
+        var detener = findViewById<Button>(R.id.Detener)
+        /* Variables para cada boton en la interfaz y uno para la cancion el cual nos pide el contexto el cual
+        sería este por eso this y la ubicación que sería en la carpeta raw
+         */
+        play.setOnClickListener{
+            cancion.start()
         }
+        pause.setOnClickListener{
+            cancion.pause()
+        }
+        detener.setOnClickListener{
+            cancion.stop()
+        }
+        /* Funciones que al clickar un boton haga su respectiva función las cuales ya vienen con el android studio
+        start para empezar la canción, pause para pararla y stop para que no se pueda reproducir de nuevo
+        */
     }
 }
