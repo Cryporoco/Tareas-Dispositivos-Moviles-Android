ac79ce9c5673cf665b209e27ad8abf2308aac4a9
Sebastian David <sebdavidpiza@gmail.com>
2024-05-31 17:50:39 -0500

    Creo el segundo fragment
    creo un fragment para agregar a un usuario

---
+++ Ejercicio Semana 12/Gestion/app/src/main/java/com/example/gestion/Datos.kt
@@ -0,0 +1,10 @@
+package com.example.gestion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class Datos : Fragment(R.layout.fragment_datos) {
+
+}
--- Ejercicio Semana 12/Gestion/app/src/main/java/com/example/gestion/Recycler.kt
+++ Ejercicio Semana 12/Gestion/app/src/main/java/com/example/gestion/Recycler.kt
@@ -5,10 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class Recycler : Fragment(R.layout.fragment_recycler) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var btnagregar = view.findViewById<Button>(R.id.btnagregar)
+        btnagregar.setOnClickListener{
+                requireActivity().supportFragmentManager.beginTransaction().apply {
+                    replace(R.id.Recycler,Datos())
+                    addToBackStack("")
+                    commit()
+                }
+        }
+    }
 
 
 }
